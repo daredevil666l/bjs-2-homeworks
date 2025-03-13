c3891c66182f5428d024e330ffafb9fc0546ca6d
Artem Gawrilov <artem.523artem@gmail.com>
2025-03-13 18:46:09 +0500

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,26 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let discriminant = b ** 2 - 4 * a * c;
+
+	if (discriminant < 0) {} else if (discriminant == 0) {
+		arr.push(-b / (2 * a));
+	} else if (discriminant > 0) {
+		arr.push(
+			(-b + Math.sqrt(discriminant)) / (2 * a),
+			(-b - Math.sqrt(discriminant)) / (2 * a)
+		);
+	}
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let monthPercent = percent / 100 / 12;
+	let change = amount - contribution;
+	let monthPay =
+		change *
+		(monthPercent + monthPercent / ((1 + monthPercent) ** countMonths - 1));
+	let AllPayments = Number((monthPay * countMonths).toFixed(2));
+	return AllPayments;
 }
